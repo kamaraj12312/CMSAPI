b54c0a083f5669ad03acf0c7d8f9c60e42bf60ec
HEMA <HEMA@DESKTOP-MQ89074>
2026-06-30 18:44:32 +0530

    com#

---
+++ CMSAPI/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMSAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        public LoginController()
+        {
+        
+        }
+
+
+    }
+}
